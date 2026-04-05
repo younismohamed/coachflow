e76610ff4720cd189d3ef80b0529ca93840f9d8a
younismohamed <younis.mohamed@tabby.sa>
2026-04-06 00:55:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: '*.supabase.co' },
-    ],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
   },
 };
-
-module.exports = nextConfig;
